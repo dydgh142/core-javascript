6de437a933fa45b22357ae6b57fc186ef2e4ea7d
dydgh <dydgh179@gmail.com>
2022-12-27 13:43:43 +0900

    환경설정 구성

---
+++ server/index.js
@@ -0,0 +1,13 @@
+
+
+
+const liverServer = require('live-server');
+
+const params = {
+    host: 'localhost',
+    port: 3000,
+    open: false,
+    root: './clinet'
+};
+
+liverServer.start(params);

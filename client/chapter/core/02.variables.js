9ac78463d275824a8d57c99eaea963b444da6cf4
dydgh <dydgh179@gmail.com>
2022-12-28 13:50:25 +0900

    05.dataType 까지

--- client/chapter/core/02.variables.js
+++ client/chapter/core/02.variables.js
@@ -1,4 +1,3 @@
-
 let admin;
 let name;
 
@@ -11,9 +10,9 @@ console.log(admin);
 /*                                      .                                     */
 /* -------------------------------------------------------------------------- */
 
-let ourPlanetName = "Earth";
+let ourPlanetName = 'Earth';
 
-let currentUserName = "John";
+let currentUserName = 'John';
 
 /* 다음 내용을 분석한 후, 프로그래밍 할 수 있도록 변수와 상수를 작성해봅니다. ————— */
 
@@ -22,24 +21,19 @@ let calcProductQuantity;
 // - 구매 제품 가격의 총 합 let
 let totalProductPrice;
 // - 1년 기준 일(day)자 수 const
-const DAYS_PER_YEAR=365;
+const DAYS_PER_YEAR = 365;
 // - 구매 결제 여부  let
 let isPayment;
 let hasClassName;
 // - 구매 결제 내역 let
 let paymentHistory;
 // - 브랜드 접두사 const
-const BRAND_PREFIX="EUID";
+const BRAND_PREFIX = 'EUID';
 // - 오늘의 운세 const
 let fortuneOfToday;
 // - 상품 정보 const
-const productInformation ='';
+const productInformation = '';
 
 /* variables ————————————————————————————— */
 
 /* constant variables ————————————————————————— */
-
-
-
-
-
--- server/index.js
+++ server/index.js
@@ -11,3 +11,7 @@ const params = {
 };
 
 liverServer.start(params);
+
+
+
+console.log(window,globalThis,self);

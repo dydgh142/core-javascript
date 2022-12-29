7df03f07d822cba11c46e7a0c7991c02e16f8f25
dydgh <dydgh179@gmail.com>
2022-12-30 00:39:48 +0900

    12.29 수업내용 08-3까지 완성

--- client/chapter/core/08-1.condition.js
+++ client/chapter/core/08-1.condition.js
@@ -2,6 +2,22 @@
 /* Condition                                                              */
 /* ---------------------------------------------------------------------- */
 
+let number = prompt('숫자를 입력해 주세요', 0);
+
+let message = number > 0 ? '1' : number < 0 ? '-1' : '아무것도 아닙니다.';
+
+console.log(message);
+
+/* 
+if(number > 0){
+  
+}else if(number < 0){
+  
+}else{
+  
+}
+ */
+
 // 그 영화 봤니?
 //     ↓
 // Yes | No
@@ -15,29 +31,23 @@ let didWatchMovie = 'yes';
 // 영화 볼거니?
 let goingToWatchMovie = 'no';
 
-// if 문(statement)
-if (didWatchMovie === 'yes') {
-  console.log('그거 재밌떠라');
+if (didWatchMovie.includes('yes')) {
+  // if 문(statement)
+  console.log('그거 재밌더라..?');
 } else if (goingToWatchMovie === 'yes') {
-  console.log('너무 설렌다');
+  // else if 복수 조건 처리
+  console.log('너무 설렌다~~');
 } else {
-  console.log('음 난 별루');
+  // else 절(caluse)
+  console.log('음...난 별루');
 }
+
+let movieMessage = didWatchMovie.includes('yes')
+  ? '그거 재밌더라'
+  : goingToWatchMovie === 'yes'
+  ? '너무 설렌다~~'
+  : '난 별루...';
 
 // 조건부 연산자
 
 // 멀티 조건부 연산자 식
-
-// let number = prompt('숫자를 입력해 주세요', 0);
-
-// if(number > 0){
-
-// }else if(number < 0){
-
-// }else{
-
-// }
-
-// let message = number > 0 ? '1' : number < 0 ? '-1' : '아무것도 아닙니다.';
-
-// console.log(message);
--- client/chapter/core/08-2.condition.js
+++ client/chapter/core/08-2.condition.js
@@ -1,19 +1,16 @@
 /* ---------------------------------------------------------------------- */
 /* Logical Operators                                                      */
 /* ---------------------------------------------------------------------- */
-let age = +prompt(alert('나이를 입력해주세요.', 0));
+
+let age = 20;
 
 if (age >= 14 && age <= 90) {
-  alert('14세 이상 90세 이하입니다.');
+  alert('14세이상 90세 이하입니다.');
 }
 
 let a = 10;
 let b = '';
 let value = Boolean(b);
-
-//{red,3}
-
-// 우선순위는 not / and / or
 
 // 논리곱(그리고) 연산자
 let AandB = a && b;
@@ -27,27 +24,25 @@ let reverseValue = !value;
 // 조건 처리
 
 // 첫번째 Falsy를 찾는 연산 (&&)
-let whichFalsy;
+let whichFalsy = true && ' ' && [] && { thisIsFalsy: false };
 
 // 첫번째 Truthy를 찾는 연산 (||)
-let whichTruthy;
+let whichTruthy = false || '' || [2, 3].length || { thisIsTruthy: true };
 
-let userName = prompt('사용자 아이디를 입력하세요.');
+let userName = prompt('사용자 아이디를 입력해주세요.', '');
 
-console.log(userName);
+if (userName === 'Admin') {
+  let pw = prompt('비밀번호를 입력해 주세요.', '');
 
-if (userName === 'admin') {
-  let pw = prompt('비밀번호 입력하세요');
-
-  if (pw === '123') {
-    console.log('환영합니다');
+  if (pw === 'TheMaster') {
+    console.log('환영합니다.');
   } else {
-    console.log('땡');
+    console.log('취소되었습니다.');
   }
 } else if (userName === '' || userName === null) {
   console.log('취소했습니다.');
 } else {
-  console.log('인증되지않은 사용자입니다.');
+  console.log('인증되지 않은 사용자 입니다.');
 }
 
-console.log(userName);
+// console.log(userName);
